b4070b7ff792fe858cb56efd1c574291aa90e40c
oyb1412 <dhdbsqja@gmail.com>
2024-03-29 14:28:18 +0900

    2024/03/29
    *시네머신, 플레이어, 무기 추가
    *플레이어 컨트롤러 추가
    *플레이어 move추가
    *플레이어 회전 추가할 예정

---
+++ Assets/MainScripts/Scens/BaseScene.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public abstract class BaseScene : MonoBehaviour
+{
+    public Define.SceneType SceneType { get; protected set; } = Define.SceneType.None;
+
+    public abstract void Clear();
+
+    public void Init()
+    {
+        var q = Managers.Instance;
+        var obj = GameObject.FindFirstObjectByType(typeof(EventSystem));
+        if (obj == null)
+            Managers.Resources.Instantiate("yb/UI/EventSystem", null).name = "@EventSystem";
+    }
+
+    private void Start()
+    {
+        Init();
+    }
+
+}
