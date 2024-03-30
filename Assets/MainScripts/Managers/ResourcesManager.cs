b43bfeafbc7893849c144d62c3f8fbd1416b14fb
윤승현 <90225322+samhomess@users.noreply.github.com>
2024-03-30 11:13:51 +0900

    윤범 코드 수정
    윤범이형 코드 수정 부분

--- Assets/MainScripts/Managers/ResourcesManager.cs
+++ Assets/MainScripts/Managers/ResourcesManager.cs
@@ -56,12 +56,9 @@ public class ResourcesManager
             Managers.Pool.Release(poolable);
             return;
         }
-        
 
-        Object.Destroy(go);
-    }
 
-        if(time == 0)
+        if (time == 0)
             Object.Destroy(go);
         else
             Object.Destroy(go, time);
