a534b5f6321247eb264acf489f30e5252c247b1f
heewoung-lee <154584396+heewoung-lee@users.noreply.github.com>
2024-04-01 14:45:24 +0900

    0401update.14:45
    플레이어 READY창 변경

--- Assets/hw/Scripts/UI/GameReadyinRoomUI.cs
+++ Assets/hw/Scripts/UI/GameReadyinRoomUI.cs
@@ -10,7 +10,7 @@ using Hashtable = ExitGames.Client.Photon.Hashtable;
 public class GameReadyinRoomUI : MonoBehaviour, IInRoomCallbacks
 {
     [SerializeField]Transform _playerStatusInGameReadyInRoomContent;//스크롤뷰 content 위치로 붙일 위치
-    [SerializeField] PlayerStatusInGameReadyInRoomSlot _playerStatusInGameReadyInRoomSlotPrefab;
+    [SerializeField] PlayerStatusInGameReadyInRoomSlot[] _playerStatusInGameReadyInRoomSlotPrefab;
     private Button _ready;
     private Button _start;
     [SerializeField] private Dictionary<string,PlayerStatusInGameReadyInRoomSlot> _playerStatusInGameReadyInRoomSlots = new Dictionary<string, PlayerStatusInGameReadyInRoomSlot>();
@@ -27,7 +27,7 @@ public class GameReadyinRoomUI : MonoBehaviour, IInRoomCallbacks
             PhotonNetwork.LocalPlayer.SetCustomProperties(
                 new Hashtable { 
                     { "isReady", !(bool)PhotonNetwork.LocalPlayer.CustomProperties["isReady"] } 
-                });//레디 버튼을 누르면 커스텀 프로퍼티가 수정 되도록 설정
+                });//레디 버튼을 누르면 커스텀 프로퍼티가 수정 반대 되도록 설정
         });
 
         _start.onClick.AddListener(() =>
@@ -51,9 +51,13 @@ public class GameReadyinRoomUI : MonoBehaviour, IInRoomCallbacks
                     return;
                 }
             }
-
             PhotonNetwork.LoadLevel("GamePlay");
         });
+
+        for(int i=0; i< _playerStatusInGameReadyInRoomSlotPrefab.Length; i++)
+        {
+            _playerStatusInGameReadyInRoomSlotPrefab[i] = transform.Find($"Panel - RoomList/Scroll View/Viewport/Content/PlayerStatusInGameReadyInRoomSlot{i}").GetComponent<PlayerStatusInGameReadyInRoomSlot>();
+        }
     }
 
     private void Start()
@@ -66,16 +70,22 @@ public class GameReadyinRoomUI : MonoBehaviour, IInRoomCallbacks
     {
         yield return new WaitUntil(() => PhotonNetwork.NetworkClientState == ClientState.Joined);//클라이언트 상태가 JOined 되면 아래 줄 실행
         yield return new WaitUntil(() => PhotonNetwork.LocalPlayer.CustomProperties.ContainsKey("isReady"));//프로퍼티 가져올때까지  대기
-        _start.gameObject.SetActive(PhotonNetwork.LocalPlayer.IsMasterClient == true);
-        _ready.gameObject.SetActive(PhotonNetwork.LocalPlayer.IsMasterClient == false);
+        _start.gameObject.SetActive(PhotonNetwork.LocalPlayer.IsMasterClient == true);//마스터 클라이언트는 _start버튼이 보이도록
+        _ready.gameObject.SetActive(PhotonNetwork.LocalPlayer.IsMasterClient == false);//마스터 클라이언트는 레디버튼이 없어짐.
 
-        Player[] players = PhotonNetwork.PlayerList;
+        Player[] players = PhotonNetwork.PlayerList;//들어갈때 플레이어 리스트를 셈
 
-        for(int i =0; i<players.Length; i++)
+     
+
+        for(int i =0; i<  players.Length; i++) //최대 프리펩 갯수 - 남은 플레이어 수 빼기
         {
-            var slot = Instantiate(_playerStatusInGameReadyInRoomSlotPrefab, _playerStatusInGameReadyInRoomContent);//스크롤뷰에 프리펩을 배치시켜 플레이어가 들어오면 자리가 생기도록 설정
-            slot.Refresh((bool)players[i].CustomProperties["isReady"]);//들어온 플레이어들의 초기 프로퍼티 셋팅
-            _playerStatusInGameReadyInRoomSlots.Add(players[i].UserId,slot);//딕셔너리에 USserID키값과 위에 설정한 slot을 넘겨줌
+            _playerStatusInGameReadyInRoomSlots.Add(players[i].UserId,
+            _playerStatusInGameReadyInRoomSlotPrefab[_playerStatusInGameReadyInRoomSlotPrefab.Length - PhotonNetwork.PlayerList.Length]);
+
+            //var slot = Instantiate(_playerStatusInGameReadyInRoomSlotPrefab, _playerStatusInGameReadyInRoomContent);//스크롤뷰에 프리펩을 배치시켜 플레이어가 들어오면 자리가 생기도록 설정
+            //slot.Refresh((bool)players[i].CustomProperties["isReady"]);//들어온 플레이어들의 초기 프로퍼티 셋팅
+            //_playerStatusInGameReadyInRoomSlots.Add(players[i].UserId,slot);//딕셔너리에 USserID키값과 위에 설정한 slot을 넘겨줌
+
         }
 
 
@@ -94,20 +104,32 @@ public class GameReadyinRoomUI : MonoBehaviour, IInRoomCallbacks
     {
         _start.gameObject.SetActive(newMasterClient.IsLocal == true);
         _ready.gameObject.SetActive(newMasterClient.IsLocal == false);
-        //Todo: 방장이 나가면 방은 그대로 두고, 모든 플레이어가 레디를 풀고, 오래있었던 다음 클라이언트가 방장이 됨.
+        //방장이 나가면 방은 그대로 두고, 모든 플레이어가 레디를 풀고, 오래있었던 다음 클라이언트가 방장이 됨.
     }
 
     public void OnPlayerEnteredRoom(Player newPlayer)//신규 플레이어가 들어왔을때 이벤트 호출, 슬롯을 만듦
     {
-        var slot = Instantiate(_playerStatusInGameReadyInRoomSlotPrefab, _playerStatusInGameReadyInRoomContent);
-        _playerStatusInGameReadyInRoomSlots.Add(newPlayer.UserId, slot);
-        StartCoroutine(C_RefreshSlot(newPlayer));
+
+
+        _playerStatusInGameReadyInRoomSlots.Add(newPlayer.UserId, 
+            _playerStatusInGameReadyInRoomSlotPrefab[_playerStatusInGameReadyInRoomSlotPrefab.Length - PhotonNetwork.PlayerList.Length]);
+        //신규 플레이어가 들어오면 슬롯자리에 신규플레이어의 딕셔너리가 추가 되도록 설정
+
+
+
+            //var slot = Instantiate(_playerStatusInGameReadyInRoomSlotPrefab, _playerStatusInGameReadyInRoomContent);//슬롯위치에 프리펩을 생성함
+            //_playerStatusInGameReadyInRoomSlots.Add(newPlayer.UserId, slot);//딕셔너리 목록에 유저 추가
+
+        //들어온 플레이어 PlayerStatusInGameReadyInRoomSlot의 배열의 순서만큼 배정.
+        //
+
+            StartCoroutine(C_RefreshSlot(newPlayer));//
     }
 
     IEnumerator C_RefreshSlot(Player player)
     {
-        yield return new WaitUntil(() => player.CustomProperties.ContainsKey("isReady"));
-        _playerStatusInGameReadyInRoomSlots[player.UserId].Refresh((bool)player.CustomProperties["isReady"]);
+        yield return new WaitUntil(() => player.CustomProperties.ContainsKey("isReady"));//신규플레이어가 커스텀프로퍼티를 가져올때까지 대시
+        _playerStatusInGameReadyInRoomSlots[player.UserId].Refresh((bool)player.CustomProperties["isReady"]);//가져오면 초기값 false로 생성
     }
 
 
@@ -115,12 +137,12 @@ public class GameReadyinRoomUI : MonoBehaviour, IInRoomCallbacks
     {
         if(_playerStatusInGameReadyInRoomSlots.TryGetValue(otherPlayer.UserId, out PlayerStatusInGameReadyInRoomSlot slot))
         {
-            Destroy(slot.gameObject);
-            _playerStatusInGameReadyInRoomSlots.Remove(otherPlayer.UserId);
+            //Destroy(slot.gameObject);//나가면 유저의 슬롯 없앰
+            _playerStatusInGameReadyInRoomSlots.Remove(otherPlayer.UserId);//나간유저의 딕셔너리 삭제
         }
     }
 
-    public void OnPlayerPropertiesUpdate(Player targetPlayer, Hashtable changedProps)
+    public void OnPlayerPropertiesUpdate(Player targetPlayer, Hashtable changedProps) // targerPlayer는 속성이 바뀐 유저, 변경된 속성을 담고 있는 해쉬테이블
     {
         if(_playerStatusInGameReadyInRoomSlots.TryGetValue(targetPlayer.UserId,out PlayerStatusInGameReadyInRoomSlot slot))
         {
