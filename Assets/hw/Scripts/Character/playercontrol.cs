d038ec6d219708be4539b89e1113bbb371c1bd35
heewoung-lee <154584396+heewoung-lee@users.noreply.github.com>
2024-04-05 07:37:35 +0900

    Update 07:36 이희웅
    GamePlay Scene 만들고, 씬전환까지 확인,
    - photonview를 저장하고 다음씬으로 넘기는건 미구현

---
+++ Assets/hw/Scripts/Character/playercontrol.cs
@@ -0,0 +1,13 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
+
+public class playercontrol : MonoBehaviourPunCallbacks
+{
+    PhotonView _view;
+    private void Start()
+    {
+    }
+}
---
+++ Assets/hw/WorkSpaceScripts/Scenes/GamePlayScene.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Hw
+{
+    public class GamePlayScene : BaseScene
+    {
+        protected override void Init()
+        {
+            base.Init();
+
+
+            //TODO 1. 맵불러온다
+            //2.Coututine 으로 맵불러온걸 체크한 다음에 플레이어를 불러온다.
+            //3.
+
+
+        }
+        public override void Clear()
+        {
+        }
+    }
+}
+
