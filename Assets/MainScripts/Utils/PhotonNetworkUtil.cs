24a76c74558e00764513c324066b63ac636e4561
heewoung-lee <154584396+heewoung-lee@users.noreply.github.com>
2024-04-09 16:01:37 +0900

    Update 0409 16:00 이희웅
    총알 동기화 수정 ver3

--- Assets/MainScripts/Utils/PhotonNetworkUtil.cs
+++ Assets/MainScripts/Utils/PhotonNetworkUtil.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class PhotonNetworkUtil : MonoBehaviourPunCallbacks
 {
     
+
     public static ProjectileMoveScript CreatePhotonObject(string name,Vector3 position,int damage,GameObject creator, Quaternion quaternion = default)
     {
         var _shotBullet = PhotonNetwork.Instantiate(name, position, quaternion).GetComponent<ProjectileMoveScript>();
         _shotBullet.Init(damage,creator);
+
+        bool isMine = _shotBullet.GetComponent<PhotonView>().IsMine;
+        if(isMine)
         _shotBullet.AddComponent<BulletLifeController>().Init();
         
 
--- Assets/yb/Scrpits/WeaponsStrategy/Projectiles/BulletLifeController.cs
+++ Assets/yb/Scrpits/WeaponsStrategy/Projectiles/BulletLifeController.cs
@@ -7,21 +7,18 @@ public class BulletLifeController : MonoBehaviour
 {
     private const float BULLET_LIFE_DEFAULT_TIME = 3.0f;
     private float _time;
-    private PhotonView _photonView;
     public void Init(float time = BULLET_LIFE_DEFAULT_TIME)
     {
         _time = time;
     }
     private void Start()
     {
-        _photonView = GetComponent<PhotonView>();
-        StartCoroutine(CoDestroyPhoton(_time));
+            StartCoroutine(CoDestroyPhoton(_time));
     }
 
     public IEnumerator CoDestroyPhoton(float time)
     {
         yield return new WaitForSeconds(time);
-        if(_photonView.IsMine)
         PhotonNetwork.Destroy(gameObject);
     }
 }
