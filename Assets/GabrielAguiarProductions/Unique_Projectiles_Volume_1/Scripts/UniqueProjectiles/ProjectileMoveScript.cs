a4a9965ac8a203210c0134c1ee2fa5136d8b9da8
heewoung-lee <154584396+heewoung-lee@users.noreply.github.com>
2024-04-09 16:25:59 +0900

    Update 16:25 0409 이희웅
    파티클 수정 업데이트 1

--- Assets/GabrielAguiarProductions/Unique_Projectiles_Volume_1/Scripts/UniqueProjectiles/ProjectileMoveScript.cs
+++ Assets/GabrielAguiarProductions/Unique_Projectiles_Volume_1/Scripts/UniqueProjectiles/ProjectileMoveScript.cs
@@ -119,10 +119,9 @@ public class ProjectileMoveScript : MonoBehaviourPunCallbacks {
         Vector3 pos = contact.point;
 
         if (hitPrefab != null) {
-            //var hitVFX = PhotonNetwork.Instantiate("Prefabs/yb/Hits/default", pos, rot) as GameObject;
-            //var ps = hitVFX.GetComponent<ParticleSystem>();
-            //ps
-            this.AddComponent<VFXLifeController>().Init();
+            var hitVFX = PhotonNetwork.Instantiate("Prefabs/yb/Hits/default", pos, rot) as GameObject;
+            var ps = hitVFX.GetComponent<ParticleSystem>();
+            hitVFX.AddComponent<VFXLifeController>().Init();
             //if (ps == null) {
             //    var psChild = hitVFX.transform.GetChild(0).GetComponent<ParticleSystem>();
             //    CoDestroyPhoton(hitVFX, psChild.main.duration);
