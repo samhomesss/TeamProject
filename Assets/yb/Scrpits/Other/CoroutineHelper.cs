db95b46286d7fdee52304ff0befaaf08d9722a31
heewoung-lee <154584396+heewoung-lee@users.noreply.github.com>
2024-04-17 16:48:57 +0900

    update

--- Assets/yb/Scrpits/Other/CoroutineHelper.cs
+++ Assets/yb/Scrpits/Other/CoroutineHelper.cs
@@ -35,9 +35,9 @@ public class CoroutineHelper : MonoBehaviour
         yield return new WaitForSeconds(time);
         GameObject go = PhotonNetwork.Instantiate("Prefabs/hw/PlayerPrefabs/Player", Vector3.zero, Quaternion.identity); 
         _photonView = Util.FindChild(go, "Model").GetComponent<PhotonView>();
-        _photonView.RPC("RenamePlayer", RpcTarget.All, _photonView.ViewID); //이름 재설정
         if (_photonView.IsMine) //카메라, 및 오디오 다시 연결
         {
+            _photonView.RPC("RenamePlayer", RpcTarget.All, _photonView.ViewID); //이름 재설정
             Util.FindChild(go, "Camera", true).SetActive(true);
             Util.FindChild(go, "Camera", true).GetComponent<AudioListener>().enabled = true;
         }
