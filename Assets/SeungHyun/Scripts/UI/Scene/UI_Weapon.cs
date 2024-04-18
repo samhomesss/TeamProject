438e4ad72ebadcfca64a0afb701c745e6f56381d
heewoung-lee <154584396+heewoung-lee@users.noreply.github.com>
2024-04-19 00:05:01 +0900

    Update UI_Weapon.cs

--- Assets/SeungHyun/Scripts/UI/Scene/UI_Weapon.cs
+++ Assets/SeungHyun/Scripts/UI/Scene/UI_Weapon.cs
@@ -60,8 +60,8 @@ public class UI_Weapon : UI_Scene
 
         if (IsTestMode.Instance.CurrentUser == Define.User.Hw)
         {
-
-            map.Player.PhotonView.RPC("Replacedweapon",RpcTarget.All, beforeItemID);
+            if (_photonView.IsMine)
+                map.Player.PhotonView.RPC("Replacedweapon",RpcTarget.All, beforeItemID);
 
 
             //GameObject go = PhotonNetwork.Instantiate($"Prefabs/yb/Weapon/{Managers.ItemDataBase.GetItemData(beforeItemID).itemName}", Vector3.zero, Quaternion.identity);
