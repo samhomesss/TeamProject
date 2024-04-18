e4f30a48e9a1999c36fb652b5519dfbf51e124e0
heewoung-lee <154584396+heewoung-lee@users.noreply.github.com>
2024-04-18 18:26:32 +0900

    update

--- Assets/SeungHyun/Scripts/UI/Scene/UI_Timer.cs
+++ Assets/SeungHyun/Scripts/UI/Scene/UI_Timer.cs
@@ -11,7 +11,7 @@ public class UI_Timer : UI_Scene
 
     public float Timer => _timer;
 
-    float _timer = 15f;
+    float _timer = 300f;
     float _minute; 
     float _second;
     static Action loadScene;
