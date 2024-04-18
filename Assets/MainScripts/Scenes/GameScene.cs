6c7a7309cbd1f373f790eb4c1b6c18d6dd675989
heewoung-lee <154584396+heewoung-lee@users.noreply.github.com>
2024-04-18 13:10:52 +0900

    update

--- Assets/MainScripts/Scenes/GameScene.cs
+++ Assets/MainScripts/Scenes/GameScene.cs
@@ -119,19 +119,17 @@ public class GameScene : BaseScene
 
     public void onLoadedUI() //로딩이 다 된다음에 호출
     {
-        if (PhotonNetwork.IsMasterClient)
-        {
+        
             for (int i = 1; i < 13; i++)
             {
                 itemBox.Add(GameObject.Find($"@Obj_Root/Map/ItemBox/DestructibleObject{i}").GetComponent<Transform>());
             }
-            for (int i = 0; i < itemBox.Count; i++)
+        for (int i = 0; i < itemBox.Count; i++)
+        {
+            if (PhotonNetwork.IsMasterClient)
             {
-                if (PhotonNetwork.IsMasterClient)
-                {
-                    GameObject itembox = PhotonNetwork.Instantiate("Prefabs/yb/Object/DestructibleObject", itemBox[i].transform.position, Quaternion.identity);
-                    itembox.transform.SetParent(_itemBox.transform);
-                }
+                GameObject itembox = PhotonNetwork.Instantiate("Prefabs/yb/Object/DestructibleObject", itemBox[i].transform.position, Quaternion.identity);
+                itembox.transform.SetParent(_itemBox.transform);
             }
         }
 
