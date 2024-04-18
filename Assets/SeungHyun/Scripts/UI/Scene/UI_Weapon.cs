c95822c6f5f657b0e0e7e6b74a9eed4304c9d923
heewoung-lee <154584396+heewoung-lee@users.noreply.github.com>
2024-04-18 23:23:45 +0900

    Update UI_Weapon.cs

--- Assets/SeungHyun/Scripts/UI/Scene/UI_Weapon.cs
+++ Assets/SeungHyun/Scripts/UI/Scene/UI_Weapon.cs
@@ -24,6 +24,8 @@ public class UI_Weapon : UI_Scene
     int _maxBullet = 15;
     int _bulletCount = 60;
 
+    GameObject ChangeWeaponObject;//
+
     string slotItemID = Define.WeaponType.Pistol.ToString(); // 처음은 피스톨로 설정 해주고
     private void Start()
     {
@@ -59,11 +61,15 @@ public class UI_Weapon : UI_Scene
 
         if (IsTestMode.Instance.CurrentUser == Define.User.Hw)
         {
+            if (PhotonNetwork.IsMasterClient)
+            {
+                ChangeWeaponObject = PhotonNetwork.Instantiate($"Prefabs/yb/Weapon/{Managers.ItemDataBase.GetItemData(beforeItemID).itemName}", Vector3.zero, Quaternion.identity);
+            }
+
             if (_photonView.IsMine)
             {
-                GameObject go = PhotonNetwork.Instantiate($"Prefabs/yb/Weapon/{Managers.ItemDataBase.GetItemData(beforeItemID).itemName}", Vector3.zero, Quaternion.identity);
-                map.Player.PhotonView.RPC("SetDropItemName", RpcTarget.All, go.GetComponent<PhotonView>().ViewID);//이름 바꾸기
-                go.transform.position = map.Player.transform.position + Vector3.up;
+                map.Player.PhotonView.RPC("SetDropItemName", RpcTarget.All, ChangeWeaponObject.GetComponent<PhotonView>().ViewID);//이름 바꾸기
+                ChangeWeaponObject.transform.position = map.Player.transform.position + Vector3.up;
             }
         }
         else
